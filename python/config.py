c29b909fc742e5242bdad067ae97459dfd8aa98b
Francis Charette-Migneault <francis.charette.migneault@gmail.com>
2024-09-27 11:48:35 -0400

    Update MLM reference with official location
    Details: https://github.com/orgs/stac-utils/discussions/4#discussioncomment-10767685

--- python/config.py
+++ python/config.py
@@ -2,7 +2,9 @@
 COMMUNITY_REPOS = [
   # org, repo name
   ['crim-ca', 'dlm-extension'],
-  ['crim-ca', 'mlm-extension'],
+  # MLM migrated to official https://github.com/stac-extensions/mlm
+  # see details: https://github.com/orgs/stac-utils/discussions/4
+  # ['crim-ca', 'mlm-extension'],
   ['Terradue', 'stac-extensions-disaster'],
   ['openrsgis', 'trainingdml-ai-extension']
 ]
