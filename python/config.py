0fd9990bcdcb706a4cee2ef591f212f6fbe05494
Jiapan Wang <jiapan.wang119@gmail.com>
2026-05-08 13:36:14 +0200

    Add topo4d STAC extension (https://github.com/tum-rsa/topo4d) (#67)
    Adds the Topographic 4D (topo4d) Extension to the community extensions list.
    
        Repo: https://github.com/tum-rsa/topo4d
        Schema: https://tum-rsa.github.io/topo4d/v0.2.0/schema.json
        Prefix: topo4d
        Scope: Item, Collection
        Maturity: Proposal
    
    The extension proposes a standardized approach for handling time-dependent metadata in diverse 4D datasets (time series of 3D geographic data), enabling STAC clients to explore and connect to multi-temporal point cloud data.

--- python/config.py
+++ python/config.py
@@ -6,7 +6,8 @@ COMMUNITY_REPOS = [
   ['openrsgis', 'trainingdml-ai-extension'],
   ['stacchain', 'merkle-tree'],
   ['IFRCGo', 'monty-stac-extension'],
-  ['cityjson', 'stac-city3d']
+  ['cityjson', 'stac-city3d'],
+  ['tum-rsa', 'topo4d']
 ]
 
 # Other extensions that are not on GitHub
